47cd9f5a3ff3587cd00208c485a0dd159c048f61
James Dong <jamesdong621@gmail.com>
2024-04-17 14:38:58 -0700

    submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,13 +22,32 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool is_valid(
+    const AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    const size_t num_days,
+    const size_t max_shifts,
+    size_t day,
+    Worker_T worker
+);
+
+bool backtrack(
+    const AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    const size_t num_days,
+    const size_t num_workers,
+    const size_t num_needed,
+    const size_t max_shifts,
+    size_t day,
+    size_t idx_worker
+);
 
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
+    const size_t num_needed,
+    const size_t max_shifts,
     DailySchedule& sched
 )
 {
@@ -38,8 +57,74 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    const size_t num_days = avail.size();
+    const size_t num_workers = avail[0].size();
 
+    sched.resize(num_days);
 
-
+    return backtrack(avail, sched, num_days, num_workers, num_needed, max_shifts, 0, 0);
 }
+
+
+bool is_valid(
+    const AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    const size_t num_days,
+    const size_t max_shifts,
+    size_t day,
+    Worker_T worker
+) {
+    if (!avail[day][worker] || find(sched[day].begin(), sched[day].end(), worker) != sched[day].end()) {
+        return false;
+    }
+
+    size_t shift_cnt = 0;
+    for (size_t d = 0; d < num_days; d++) {
+        if (find(sched[d].begin(), sched[d].end(), worker) != sched[d].end()) {
+            shift_cnt++;
+        }
+    }
+
+    return shift_cnt < max_shifts;
+}
+
+bool backtrack(
+    const AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    const size_t num_days,
+    const size_t num_workers,
+    const size_t num_needed,
+    const size_t max_shifts,
+    size_t day,
+    size_t idx_worker
+) {
+    size_t next_day = day;
+    size_t next_idx_worker = idx_worker;
+
+    bool end = false;
+    if (idx_worker + 1 < num_needed) {
+        next_idx_worker = idx_worker + 1;
+    } else if (day + 1 < num_days) {
+        next_idx_worker = 0;
+        next_day = day + 1;
+    } else {
+        end = true;
+    }
+
+    for (Worker_T worker = 0; worker < num_workers; worker++) {
+        if (is_valid(avail, sched, num_days, max_shifts, day, worker)) {
+            sched[day].push_back(worker);
+            if (end) {
+                return true;
+            }
+            if (backtrack(avail, sched, num_days, num_workers, num_needed, max_shifts, next_day, next_idx_worker)) {
+                return true;
+            }
+            sched[day].pop_back();
+        }
+    }
+
+    return false;
+}
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -15,6 +15,20 @@ using namespace std;
 // Add prototypes of helper functions here
 
 
+void gen_map(const std::string& str, std::map<char, int>& str_map);
+
+
+void gen_words(
+    const std::string& in,
+    const std::string& floating,
+    size_t floating_iter,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result,
+    std::string& current,
+    size_t length,
+    size_t empty_index
+);
+
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +36,56 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> result;
+    
+    std::string current(in);
+    
+    size_t cnt = 0;
+    for (auto c: in) {
+        if (c != '-') {
+            cnt++;
+        }
+    }
 
+    gen_words(in, floating, 0, dict, result, current, cnt, 0);
+    return result;
 }
 
 // Define any helper functions here
+
+void gen_words(
+    const std::string& in,
+    const std::string& floating,
+    size_t floating_iter,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result,
+    std::string& current,
+    size_t length,
+    size_t empty_index
+) {
+    if (length >= in.length()) {
+        if (dict.find(current) != dict.end()) {
+            result.insert(current);
+        }
+        return;
+    }
+
+    if (floating_iter < floating.length()) {
+        for (size_t i = 0; i < in.length(); i++) {
+            if (current[i] == '-') {
+                current[i] = floating[floating_iter];
+                gen_words(in, floating, floating_iter + 1, dict, result, current, length + 1, empty_index);
+                current[i] = '-';
+            }
+        }
+    } else {
+        while (current[empty_index] != '-') {
+            empty_index++;
+        }
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            current[empty_index] = ch;
+            gen_words(in, floating, floating_iter, dict, result, current, length + 1, empty_index);
+            current[empty_index] = '-';
+        }
+    }
+}
